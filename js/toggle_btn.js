8ee5b2eb346af9a0f3bc17f46e9fe8684b7f3426
kimkihong1018 <rhflffk101809@gmail.com>
2024-07-19 12:09:32 +0900

    last_homepage_file

--- js/toggle_btn.js
+++ js/toggle_btn.js
@@ -2,4 +2,15 @@ $(function(){
     $(".menu_toggle_btn").click(function(){
         $(".gmb").stop().slideToggle("fast")
     })
+
+
+    $(".search_select_box span").hover(function(){
+        $(".search_select_list").stop().slideDown();
+        //$(this).next().stop().slideDown();
+    },function(){
+        $(".search_select_list").stop().slideUp();
+         //$(this).next().stop().slideUp();
+    })
+
+
 })//jQ end
